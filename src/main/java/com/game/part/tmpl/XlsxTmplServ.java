0e9a9ee2ae1b9bed42ae11d96275d06221cf6ff7
hjj2017 <hjj2017@163.com>
2015-12-30 22:43:44 +0800

    添加 Xgame-part Template 项目；

---
+++ src/main/java/com/game/part/tmpl/XlsxTmplServ.java
@@ -0,0 +1,60 @@
+package com.game.part.tmpl;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.game.part.util.Assert;
+
+
+/**
+ * Excel 模板服务
+ * 
+ * @author hjj2017
+ * @since 2014/6/6
+ * 
+ */
+public class XlsxTmplServ implements IServ_LoadTmplData, IServ_PackUp, IServ_Validate {
+	/** 单例对象 */
+	public static final XlsxTmplServ OBJ = new XlsxTmplServ();
+	/** Excel 文件所在目录 */
+	public String _xlsxFileDir = null;
+
+	/**
+	 * 属性字典,
+	 * <font color="#990000">但目前只被应用于 &#64;FromXlsxFile 注解!</font>
+	 *
+	 * @see com.game.part.tmpl.anno.FromXlsxFile
+	 *
+	 */
+	public Map<String, String> _propMap = null;
+
+	/** 输出类文件到目标目录, 主要用于调试 */
+	public String _debugClazzToDir = null;
+	/** 对象列表字典 */
+	final ConcurrentHashMap<Class<?>, List<?>> _objListMap = new ConcurrentHashMap<>();
+
+	/**
+	 * 类默认构造器
+	 * 
+	 */
+	private XlsxTmplServ() {
+	}
+
+	/**
+	 * 获取对象列表
+	 * 
+	 * @param clazz
+	 * @return 
+	 * 
+	 */
+	public <T> List<T> getObjList(Class<T> clazz) {
+		// 断言参数不为空
+		Assert.notNull(clazz, "clazz");
+
+		// 获取模板列表
+		@SuppressWarnings("unchecked")
+		List<T> tl = (List<T>)this._objListMap.get(clazz);
+		return tl;
+	}
+}
